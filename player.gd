a2e761ac6c8a64dc98cf72bcc1c486f92e331508
TheExpert24 <express.vanukuri@gmail.com>
2026-06-05 08:42:05 -0700

    fixed the view point and the arrow keys

--- player.gd
+++ player.gd
@@ -28,9 +28,9 @@ func _physics_process(delta):
 	var input_dir = Vector2.ZERO
 
 	if Input.is_action_pressed("ui_up"):
-		input_dir.y -= 1
-	if Input.is_action_pressed("ui_down"):
 		input_dir.y += 1
+	if Input.is_action_pressed("ui_down"):
+		input_dir.y -= 1
 	if Input.is_action_pressed("ui_left"):
 		input_dir.x -= 1
 	if Input.is_action_pressed("ui_right"):
